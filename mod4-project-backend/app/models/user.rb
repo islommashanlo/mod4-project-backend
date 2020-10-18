6c89bb6c6c2d627b74829113f41930ad6ee16416
royFloresNyc <royfloresnyc@gmail.com>
2020-10-18 16:35:01 -0400

    Modified rendering of address and geo location

--- mod4-project-backend/app/models/user.rb
+++ mod4-project-backend/app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
 # -----------------------------------------------------------------------------------
     validates :username, uniqueness: { case_sensitive: false }, length: {maximum: 50}
 # -----------------------------------------------------------------------------------
+
+    def self.api_call(url)
+        uri = URI(url)
+        response = Net::HTTP.get_response(uri)
+        json_response = JSON.parse(response.read_body)    
+    end
+
 end
--- mod4-project-backend/app/serializers/user_serializer.rb
+++ mod4-project-backend/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :id, :username, :first_name, :last_name, :address, :account_balance, :banks, :transaction_types, :credit_categories, :debit_categories, :transactions
+    attributes :id, :username, :first_name, :last_name, :address, :account_balance, :banks, :transaction_types, :credit_categories, :debit_categories, :transactions, :geo_location, :q_address
 
     def account_balance
         self.object.transactions.select{|e|e.transaction_type_id == 1}.sum{|e|e.amount} - self.object.transactions.select{|e|e.transaction_type_id == 2}.sum{|e|e.amount}
@@ -33,4 +33,11 @@ class UserSerializer < ActiveModel::Serializer
         TransactionType.all
     end
 
+    def geo_location
+        User.api_call("https://maps.googleapis.com/maps/api/geocode/json?address=#{self.object.address}&key=#{ENV["MAPS_KEY"]}")['results'][0]['geometry']['location']
+    end 
+
+    def q_address
+        self.object.address.tr(" ", "+")
+    end 
 end
