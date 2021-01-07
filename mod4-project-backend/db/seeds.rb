bfce25f97054575134ad2db5b222dc6c18ddb251
royFloresNyc <royfloresnyc@gmail.com>
2021-01-07 17:53:53 -0500

    Edit Seed Data

--- mod4-project-backend/db/seeds.rb
+++ mod4-project-backend/db/seeds.rb
@@ -6,113 +6,98 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-isa = User.create(username: "isa", first_name: "Isa", last_name: "Akhmedov", address: "7205 17th Avenue, Brooklyn, NY", password: "test", password_confirmation: "test")
+roy = User.create(username: "royFloresNyc", first_name: "Roy", last_name: "Flores", address: "3612, Broadway, NY", password: "test", password_confirmation: "test")
 
 # Transaction Type
 credit = TransactionType.create(name: "Credit")
 debit = TransactionType.create(name: "Debit")
 # Credit Type Categories 
-income = Category.create(name:"Income", user_id: isa.id, transaction_type_id: credit.id)
-refund = Category.create(name:"Refunds", user_id: isa.id, transaction_type_id: credit.id)
-gift = Category.create(name:"Gifts", user_id: isa.id, transaction_type_id: credit.id)
-misc_inc = Category.create(name:"Miscellaneous", user_id: isa.id, transaction_type_id: credit.id)
+income = Category.create(name:"Income", user_id: roy.id, transaction_type_id: credit.id)
+refund = Category.create(name:"Refunds", user_id: roy.id, transaction_type_id: credit.id)
+gift = Category.create(name:"Gifts", user_id: roy.id, transaction_type_id: credit.id)
+misc_inc = Category.create(name:"Miscellaneous", user_id: roy.id, transaction_type_id: credit.id)
 # Debit Type Category
-bills = Category.create(name:"Bills", user_id: isa.id, transaction_type_id: debit.id)
-shop = Category.create(name:"Groceries", user_id: isa.id, transaction_type_id: debit.id)
-ent = Category.create(name:"Entertainment", user_id: isa.id, transaction_type_id: debit.id)
-din = Category.create(name:"Dining Out", user_id: isa.id, transaction_type_id: debit.id)
-transp = Category.create(name:"Transportation", user_id: isa.id, transaction_type_id: debit.id)
-misc = Category.create(name:"Miscellaneous", user_id: isa.id, transaction_type_id: debit.id)
+bills = Category.create(name:"Bills", user_id: roy.id, transaction_type_id: debit.id)
+shop = Category.create(name:"Groceries", user_id: roy.id, transaction_type_id: debit.id)
+ent = Category.create(name:"Entertainment", user_id: roy.id, transaction_type_id: debit.id)
+din = Category.create(name:"Dining Out", user_id: roy.id, transaction_type_id: debit.id)
+transp = Category.create(name:"Transportation", user_id: roy.id, transaction_type_id: debit.id)
+misc = Category.create(name:"Miscellaneous", user_id: roy.id, transaction_type_id: debit.id)
 
 # User
-isa_bank = Bank.create(name: "Chase")
-isa_acc = Account.create(user_id: isa.id, bank_id: isa_bank.id)
+roy_bank = Bank.create(name: "Chase")
+roy_acc = Account.create(user_id: roy.id, bank_id: roy_bank.id)
+
+# Date generator
+def getRandomPrevDate(numOfMonthAgo)
+    date = DateTime.now - (((numOfMonthAgo - 1) * 30) + (rand * 30))
+    return date.strftime('%Y-%m-%d')
+end 
 
 # Transactions
- #Month 1
-transact1 = Transaction.create(name: "Bills", amount: 500.00, t_date: "2020-01-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-transact2 = Transaction.create(name: "Rent", amount: 1500.00, t_date: "2020-01-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-transact3 = Transaction.create(name: "Shopping", amount: 100.00, t_date: "2020-01-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-transact4 = Transaction.create(name: "Movies", amount: 100.00, t_date: "2020-01-01", category_id: ent.id, transaction_type_id: debit.id, user_id: isa.id)
-transact5 = Transaction.create(name: "Restaurant", amount: 100.00, t_date: "2020-01-01", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-transact6 = Transaction.create(name: "Train", amount: 100.00, t_date: "2020-01-01", category_id: transp.id, transaction_type_id: debit.id, user_id: isa.id)
-transact7 = Transaction.create(name: "Gift", amount: 100.00, t_date: "2020-01-01", category_id: misc.id, transaction_type_id: debit.id, user_id: isa.id)
+ #3 Months Ago
+Transaction.create(name: "Salary", amount: 5000.00, t_date: "#{(DateTime.now - 60).year}-#{(DateTime.now - 90).strftime('%m')}-01", category_id: income.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Rite Aid", amount: 1.50, t_date: getRandomPrevDate(3), category_id: refund.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Check from Nana", amount: 100.00, t_date: getRandomPrevDate(3), category_id: gift.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Acct Dividend", amount: 6.66, t_date: getRandomPrevDate(3), category_id: misc_inc.id, transaction_type_id: credit.id, user_id: roy.id)
 
-transact8 = Transaction.create(name: "Salary", amount: 10000.00, t_date: "2020-01-01", category_id: income.id, transaction_type_id: credit.id, user_id: isa.id)
-transact9 = Transaction.create(name: "Refund", amount: 0.50, t_date: "2020-01-01", category_id: refund.id, transaction_type_id: credit.id, user_id: isa.id)
-transact10 = Transaction.create(name: "Present", amount: 1.00, t_date: "2020-01-01", category_id: gift.id, transaction_type_id: credit.id, user_id: isa.id)
-transact11 = Transaction.create(name: "Portfolio", amount: 6.66, t_date: "2020-01-01", category_id: misc_inc.id, transaction_type_id: credit.id, user_id: isa.id)
-#Month 2
-Transaction.create(name: "Bills", amount: 500.00, t_date: "2020-02-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Rent", amount: 1500.00, t_date: "2020-02-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 100.00, t_date: "2020-02-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Movies", amount: 100.00, t_date: "2020-02-01", category_id: ent.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 100.00, t_date: "2020-02-01", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Train", amount: 100.00, t_date: "2020-02-01", category_id: transp.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Gift", amount: 100.00, t_date: "2020-02-01", category_id: misc.id, transaction_type_id: debit.id, user_id: isa.id)
+Transaction.create(name: "Rent", amount: 1500.00, t_date: "#{(DateTime.now - 60).year}-#{(DateTime.now - 90).strftime('%m')}-01", category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Student Loan", amount: 500.00, t_date: getRandomPrevDate(3), category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "WholeFoods", amount: 98.00, t_date: getRandomPrevDate(3), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Concert", amount: 100.00, t_date: getRandomPrevDate(3), category_id: ent.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "The Smith", amount: 87.98, t_date: getRandomPrevDate(3), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "MTA Card", amount: 100.00, t_date: getRandomPrevDate(3), category_id: transp.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Gift for Jordan", amount: 100.00, t_date: getRandomPrevDate(3), category_id: misc.id, transaction_type_id: debit.id, user_id: roy.id)
 
-Transaction.create(name: "Salary", amount: 10000.00, t_date: "2020-02-01", category_id: income.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Refund", amount: 0.50, t_date: "2020-02-01", category_id: refund.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Present", amount: 1.00, t_date: "2020-02-01", category_id: gift.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Portfolio", amount: 6.66, t_date: "2020-02-01", category_id: misc_inc.id, transaction_type_id: credit.id, user_id: isa.id)
-#Month 3
-Transaction.create(name: "Bills", amount: 500.00, t_date: "2020-03-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Rent", amount: 1500.00, t_date: "2020-03-01", category_id: bills.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 100.00, t_date: "2020-03-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Movies", amount: 100.00, t_date: "2020-03-01", category_id: ent.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 100.00, t_date: "2020-03-01", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Train", amount: 100.00, t_date: "2020-03-01", category_id: transp.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Gift", amount: 100.00, t_date: "2020-03-01", category_id: misc.id, transaction_type_id: debit.id, user_id: isa.id)
+#2 Months Ago
+Transaction.create(name: "Salary", amount: 5000.00, t_date: "#{(DateTime.now - 30).year}-#{(DateTime.now - 60).strftime('%m')}-01", category_id: income.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Duane Reade", amount: 0.50, t_date: getRandomPrevDate(2), category_id: refund.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Check from Mom", amount: 20.00, t_date: getRandomPrevDate(2), category_id: gift.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Acct Dividend", amount: 6.66, t_date: getRandomPrevDate(2), category_id: misc_inc.id, transaction_type_id: credit.id, user_id: roy.id)
 
-Transaction.create(name: "Salary", amount: 10000.00, t_date: "2020-03-01", category_id: income.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Refund", amount: 0.50, t_date: "2020-03-01", category_id: refund.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Present", amount: 1.00, t_date: "2020-03-01", category_id: gift.id, transaction_type_id: credit.id, user_id: isa.id)
-Transaction.create(name: "Portfolio", amount: 6.66, t_date: "2020-03-01", category_id: misc_inc.id, transaction_type_id: credit.id, user_id: isa.id)
+Transaction.create(name: "Rent", amount: 1500.00, t_date: "#{(DateTime.now - 30).year}-#{(DateTime.now - 60).strftime('%m')}-01", category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Student Loan", amount: 500.00, t_date: getRandomPrevDate(2), category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "WholeFoods", amount: 100.00, t_date: getRandomPrevDate(2), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Movies", amount: 80.00, t_date: getRandomPrevDate(2), category_id: ent.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "River Thai", amount: 30.00, t_date: getRandomPrevDate(2), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "MTA Card", amount: 100.00, t_date: getRandomPrevDate(2), category_id: transp.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Gift", amount: 100.00, t_date: getRandomPrevDate(2), category_id: misc.id, transaction_type_id: debit.id, user_id: roy.id)
 
+#Within the last Month
+Transaction.create(name: "Salary", amount: 5000.00, t_date: "#{DateTime.now.year}-#{DateTime.now.strftime('%m')}-01", category_id: income.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Amazon", amount: 5.00, t_date: getRandomPrevDate(1), category_id: refund.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Venmo from David", amount: 50.00, t_date: getRandomPrevDate(1), category_id: gift.id, transaction_type_id: credit.id, user_id: roy.id)
+Transaction.create(name: "Acct Dividend", amount: 6.66, t_date: getRandomPrevDate(1), category_id: misc_inc.id, transaction_type_id: credit.id, user_id: roy.id)
 
-Transaction.create(name: "Restaurant", amount: 100.00, t_date: "2020-03-12", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 110.00, t_date: "2020-03-13", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 121.00, t_date: "2020-03-14", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 155.00, t_date: "2020-03-15", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 168.00, t_date: "2020-03-16", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 200.00, t_date: "2020-03-17", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Restaurant", amount: 250.00, t_date: "2020-03-18", category_id: din.id, transaction_type_id: debit.id, user_id: isa.id)
+Transaction.create(name: "Rent", amount: 1500.00, t_date: "#{DateTime.now.year}-#{DateTime.now.strftime('%m')}-01", category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Student Loan", amount: 500.00, t_date: getRandomPrevDate(1), category_id: bills.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "WholeFoods", amount: 70.00, t_date: getRandomPrevDate(1), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Mulan on iTunes", amount: 25.00, t_date: getRandomPrevDate(1), category_id: ent.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "The Smith", amount: 98.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "MTA Card", amount: 100.00, t_date: getRandomPrevDate(1), category_id: transp.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Venmo to David", amount: 100.00, t_date: getRandomPrevDate(1), category_id: misc.id, transaction_type_id: debit.id, user_id: roy.id)
 
-Transaction.create(name: "Shopping", amount: 100.00, t_date: "2020-01-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 90.00, t_date: "2020-02-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 80.00, t_date: "2020-03-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 70.00, t_date: "2020-04-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
-Transaction.create(name: "Shopping", amount: 33.00, t_date: "2020-05-01", category_id: shop.id, transaction_type_id: debit.id, user_id: isa.id)
+Transaction.create(name: "The Smith", amount: 100.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "HandPulled Noodles", amount: 45.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "River Thai", amount: 31.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Jan Jad Vietnamese", amount: 55.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "The Empire", amount: 168.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Starbucks", amount: 15.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Mamasushi", amount: 150.00, t_date: getRandomPrevDate(1), category_id: din.id, transaction_type_id: debit.id, user_id: roy.id)
+
+Transaction.create(name: "Amazon Fresh", amount: 100.00, t_date: getRandomPrevDate(1), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "WholeFoods", amount: 90.00, t_date: getRandomPrevDate(2), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Amazon Prime", amount: 80.00, t_date: getRandomPrevDate(1), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "WholeFoods", amount: 70.00, t_date: getRandomPrevDate(3), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
+Transaction.create(name: "Food Town", amount: 33.00, t_date: getRandomPrevDate(3), category_id: shop.id, transaction_type_id: debit.id, user_id: roy.id)
 
 
 
 # Budget
-Budget.create(name: bills.name, amount: 2000, category_id: bills.id, user_id: isa.id)
-Budget.create(name: shop.name, amount: 200, category_id: shop.id, user_id: isa.id)
-Budget.create(name: ent.name, amount: 200, category_id: ent.id, user_id: isa.id)
-Budget.create(name: din.name, amount: 100, category_id: din.id, user_id: isa.id)
-Budget.create(name: transp.name, amount: 100, category_id: transp.id, user_id: isa.id)
-Budget.create(name: misc.name, amount: 100, category_id: misc.id, user_id: isa.id)
+Budget.create(name: bills.name, amount: 2000, category_id: bills.id, user_id: roy.id)
+Budget.create(name: shop.name, amount: 200, category_id: shop.id, user_id: roy.id)
+Budget.create(name: ent.name, amount: 200, category_id: ent.id, user_id: roy.id)
+Budget.create(name: din.name, amount: 100, category_id: din.id, user_id: roy.id)
+Budget.create(name: transp.name, amount: 100, category_id: transp.id, user_id: roy.id)
+Budget.create(name: misc.name, amount: 100, category_id: misc.id, user_id: roy.id)
 
-
-
-
-
-
-# def api_call(url)
-#     uri = URI(url)
-#     response = Net::HTTP.get_response(uri)
-#     json_response = JSON.parse(response.read_body)    
-    
-# end
-
-
-
-
-# byebug
-
-
-# api_call("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.6162787,-73.9987585&radius=500&keyword=atm&key=#{ENV["API_KEY"]}")
-
-# # api_call("https://maps.googleapis.com/maps/api/geocode/json?address=7205+17th+Avenue,+Brooklyn,+NY&key=#{ENV["API_KEY"]}")
-# api_call("https://maps.googleapis.com/maps/api/geocode/json?address=7205+17th+Avenue,+Brooklyn,+NY&key=#{ENV["API_KEY"]}")
